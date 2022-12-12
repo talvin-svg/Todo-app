fb6261996dd14ad1670db1a3b4c188701b2fad1c
Talvin <92906931+ThroughTeeslenS@users.noreply.github.com>
2022-12-12 01:18:53 -0500

    ../

---
+++ .history/lib/components/app_text_input_field_20221208000446.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'apptextfield.dart';
+
+class AppRichTextInputField extends StatelessWidget {
+  final String hintText;
+  final bool obscureText;
+  final TextEditingController controller;
+  final int? maxLines;
+  final Icon? suffixIcon;
+  final void Function()? suffixFunction;
+  final Color? color;
+  final Color? hintColor;
+
+  const AppRichTextInputField(
+    BuildContext context, {
+    super.key,
+    required this.hintText,
+    required this.controller,
+    this.obscureText = false,
+    this.maxLines,
+    this.suffixFunction,
+    this.suffixIcon,
+    this.color,
+    this.hintColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface,
+          borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: AppTextField(
+          padding: const EdgeInsets.all(10),
+          controller: controller,
+          hintText: hintText,
+          fontSize: 14,
+          obscureText: obscureText,
+          maxLines: maxLines,
+          suffixFunction: suffixFunction,
+          suffixIcon: suffixIcon,
+          color: color,
+          hintColor: hintColor,
+        ),
+      ),
+    );
+  }
+}
---
+++ .history/lib/screens/welcome_screen_20221207120450.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import '/components/apptextfield.dart';
+import '../components/app_text.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: const SignUpPage(),
+    );
+  }
+}
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  bool showPassword = false;
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Theme.of(context).backgroundColor,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/todo-logo.png'),
+            const Expanded(
+              child: AppText(text: 'The right app to get work done'),
+            ),
+            Expanded(
+                child: Form(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const AppText(
+                    text: 'USERNAME',
+                  ),
+                  const SizedBox(height: 10),
+                  AppTextField(
+                    controller: _usernameController,
+                  ),
+                  AppTextField(controller: _passwordController)
+                ],
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
